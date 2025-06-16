83f11e514d80aa40a1edcf4dd03db8afccdd7039
Lis <168233558+marialis19@users.noreply.github.com>
2025-06-16 17:05:52 -0300

    Solucionamos despliegue navbar

--- assets/components/java.js
+++ assets/components/java.js
@@ -1,13 +1,26 @@
- document.addEventListener("DOMContentLoaded", function () {
-      fetch("../assets/components/navbar.html")
-        .then(response => response.text())
-        .then(data => {
-          document.getElementById("navbar").innerHTML = data;
-        });
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("../assets/components/navbar.html")
+    .then((response) => response.text())
+    .then((data) => {
+      const navbar = document.getElementById("navbar");
+      navbar.innerHTML = data;
 
-      fetch("../assets/components/footer.html")
-        .then(response => response.text())
-        .then(data => {
-          document.getElementById("footer").innerHTML = data;
+      // Cierra el menú hamburguesa al hacer clic en un enlace
+      navbar.querySelectorAll(".nav-link").forEach((link) => {
+        link.addEventListener("click", () => {
+          const navbarCollapse = navbar.querySelector(".navbar-collapse");
+          if (navbarCollapse.classList.contains("show")) {
+            new bootstrap.Collapse(navbarCollapse).toggle();
+          }
         });
+      });
     });
+
+  fetch("../assets/components/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      document.getElementById("footer").innerHTML = data;
+    });
+});
+
+
